68b413153543126ba747bbb500919b5f7721c59c
Mlabuschagne <89184337+Mlabuschagne@users.noreply.github.com>
2021-08-20 09:01:00 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticalMarno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
